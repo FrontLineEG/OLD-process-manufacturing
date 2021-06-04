3ab6e6c6220bb2d0c06d2830a94625949aab632e
root <root@ip-172-31-11-121.eu-central-1.compute.internal>
2021-06-04 16:06:39 +0000

    add custom script om work order2

--- process_manufacturing/process_manufacturing/doctype/work_order_item2/work_order_item2.py
+++ process_manufacturing/process_manufacturing/doctype/work_order_item2/work_order_item2.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018, earthians and contributors
+# Copyright (c) 2021, earthians and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+# import frappe
 from frappe.model.document import Document
 
-class ProcessOrderItem(Document):
+class WorkOrderItem2(Document):
 	pass
